8017fd70dfc08e5142e0ce8a63d65937c4c4010b
Eric Pandolfo <eric.pandolfo@gmail.com>
2022-03-25 16:57:54 -0400

    Adicionando o @Repository

--- src/main/java/br/com/siecola/aws_project01/repository/InvoiceRepository.java
+++ src/main/java/br/com/siecola/aws_project01/repository/InvoiceRepository.java
@@ -2,9 +2,11 @@ package br.com.siecola.aws_project01.repository;
 
 import br.com.siecola.aws_project01.entity.Invoice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
 
     Invoice findByInvoiceNumber(String invoiceNumber);
--- src/main/java/br/com/siecola/aws_project01/repository/ProductRepository.java
+++ src/main/java/br/com/siecola/aws_project01/repository/ProductRepository.java
@@ -2,9 +2,11 @@ package br.com.siecola.aws_project01.repository;
 
 import br.com.siecola.aws_project01.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     Optional<Product> findByCode(String code);
